067ca03118edf40964292291eff3fd9802ef6897
999nowa <131688612+999nowa@users.noreply.github.com>
2026-08-25 14:38:26 +0200

    Copy GoogleSatelliteMapController from utgick

---
+++ OsmAnd/src/net/osmand/plus/googlemaps/GoogleSatelliteMapController.java
@@ -0,0 +1,40 @@
+package net.osmand.plus.googlemaps;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.FrameLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+/** Experimental bridge for a Google Maps satellite map. */
+public final class GoogleSatelliteMapController {
+    private static final String MAP_VIEW_CLASS = "com.google.android.gms.maps.MapView";
+    private static final String MAP_OPTIONS_CLASS = "com.google.android.gms.maps.GoogleMapOptions";
+    private static final String MAP_TYPE_SATELLITE = "MAP_TYPE_SATELLITE";
+    private GoogleSatelliteMapController() {}
+    @Nullable
+    public static View createMapView(@NonNull Context context) {
+        try {
+            Class<?> optionsClass = Class.forName(MAP_OPTIONS_CLASS);
+            Object options = optionsClass.getDeclaredConstructor().newInstance();
+            Method mapType = optionsClass.getMethod("mapType", int.class);
+            mapType.invoke(options, resolveSatelliteType());
+            Class<?> mapViewClass = Class.forName(MAP_VIEW_CLASS);
+            Constructor<?> constructor = mapViewClass.getConstructor(Context.class, optionsClass);
+            return (View) constructor.newInstance(context, options);
+        } catch (Throwable ignored) { return null; }
+    }
+    public static boolean attachSatelliteMap(@NonNull FrameLayout container) {
+        View mapView = createMapView(container.getContext());
+        if (mapView == null) return false;
+        container.removeAllViews();
+        container.addView(mapView, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+        return true;
+    }
+    private static int resolveSatelliteType() throws Exception {
+        Class<?> googleMapClass = Class.forName("com.google.android.gms.maps.GoogleMap");
+        return googleMapClass.getField(MAP_TYPE_SATELLITE).getInt(null);
+    }
+}
