713787b9190bcbed7258664c19c7698e8ba1dcea
999nowa <131688612+999nowa@users.noreply.github.com>
2026-08-25 14:38:31 +0200

    Copy GoogleSearchSettings from utgick

---
+++ OsmAnd/src/net/osmand/plus/search/GoogleSearchSettings.java
@@ -0,0 +1,7 @@
+package net.osmand.plus.search;
+
+/** Runtime configuration for the optional Google address search provider. */
+public final class GoogleSearchSettings {
+    public enum ProviderMode { OSMAND_ONLY, GOOGLE_ONLY, BOTH }
+    private GoogleSearchSettings() {}
+}
